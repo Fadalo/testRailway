38e405991ced77e5a9b26a41b94f5ceba55eeae1
Fadalo <setiawan.ricky@gmail.com>
2023-06-06 14:12:57 +0700

    change index port

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
 
 app.post('/')
 
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+app.listen(port,"0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`)
 })
