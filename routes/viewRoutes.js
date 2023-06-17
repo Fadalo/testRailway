bce408835bb6a5358d0f74456c122a6430b9630b
Fadalo <setiawan.ricky@gmail.com>
2023-06-18 06:27:10 +0700

    fix session

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -38,7 +38,7 @@ app.post('/login',urlencodedParser, async (req, res) => {
     url:     'http://localhost:3000/api/login',
     body:    "emailID="+req.body.emailID+"&password="+req.body.password
   }, function(error, response, body){
-    if(!empty(body) ){ res.redirect('/login'); }
+    if(body == "" ){ res.redirect('/login'); }
     var resultP = JSON.parse(body)   ; 
     if(resultP.result == "ok" && resultP.cid != ""){
       req.session.currUser = resultP.cid;
