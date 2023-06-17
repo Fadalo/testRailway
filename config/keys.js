e232ccaec4f2d2380c65cacd6eaee026d7810acd
Fadalo <setiawan.ricky@gmail.com>
2023-06-18 06:06:14 +0700

    fix mongouri

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
+const mongoURI = process.env.PORT || "mongodb://localhost:27017/lms_db";
 module.exports = {
-   // mongoURI: 'mongodb+srv://Ricky:Sansan87@cluster0.qqmbzlz.mongodb.net/lms_db',
-   mongoURI:'mongodb://localhost:27017/lms_db', 
+   //mongoURI: 'mongodb+srv://Ricky:Sansan87@cluster0.qqmbzlz.mongodb.net/lms_db',
+   mongoURI: mongoURI, 
    cookieKey: '928304nnd003jdailajsdh',
     pages: {'title':'lms simplylearn final project','currentPages':'','prevPages':''},
 };
